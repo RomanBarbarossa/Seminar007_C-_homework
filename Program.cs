0786e7de1983b4ec5577baca01915c7c64b283ed
RomanBarbarossa <roman.barbarossa@mail.ru>
2023-01-29 20:04:23 +0300

    end

--- Program.cs
+++ Program.cs
@@ -40,8 +40,7 @@
 //     }
 // }
 
-// _________________________________________________________-
-
+// ---------------------------------------------------------------
 // Задача 50. Напишите программу, которая на вход принимает число,и проверяет есть ли 
 // такое число в двумерном массиве, а также возвращает сообщение о том,что оно найдено 
 // или же указание, что такого элемента нет.
@@ -50,6 +49,77 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+// ____________-----Methods-----______________
+
+//           __1__  создание массива
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] res = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             res[i, j] = new Random().Next(min, max - 1);
+//         }
+//     }
+//     return res;
+// }
+// //          __2__ печать данного массива
+// void PrintArray(int[,] col)
+// {
+//     for (int i = 0; i < col.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < col.GetLength(1); j++)
+//         {
+//             Console.Write($"{col[i, j]}  ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// // __3__ нахождение нужного значения в данном массиве
+
+// void GetNumFromArray(int[,] array, int find)
+// {
+//     bool IsHere = false;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j] == find) IsHere = true;
+//         }
+//     }
+//     if (IsHere == true)
+//     {
+//         Console.WriteLine("Число есть в массиве");
+//     }
+//     else Console.WriteLine("Числа нет в массиве");
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int m = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите количество столбцов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+
+// int[,] ourArray = GetArray(m, n, -5, 23);
+// PrintArray(ourArray);
+
+// Console.Write("Введите искомое число: ");
+// int find = int.Parse(Console.ReadLine()!);
+
+// GetNumFromArray(ourArray, find);
+
+
+// ---------------------------------------------------------------
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
 
 // ____________-----Methods-----______________
 
@@ -79,35 +149,29 @@ void PrintArray(int[,] col)
     }
 }
 
-        // __3__ нахождение нужного значения в данном массиве
+//          __3__ нахождение среднего арифметического в СТОЛБЦЕ
 
-void GetNumFromArray(int[,] array, int find)
+void AverageInColumn(int[,] array)
 {
-    bool IsHere = false;
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(1); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        double sumColumn = 0;
+        for (int j = 0; j < array.GetLength(0); j++)
         {
-            if (array[i, j] == find) IsHere = true;
+            sumColumn = sumColumn + array[j, i];
         }
+        Console.Write(Convert.ToDouble(sumColumn / array.GetLength(0)) + " |");
     }
-    if (IsHere == true)
-    {
-        Console.WriteLine("Число есть в массиве");
-    }
-    else Console.WriteLine("Числа нет в массиве");
+
 }
 
 Console.Write("Введите количество строк массива: ");
-int m = int.Parse(Console.ReadLine () !);
+int m = int.Parse(Console.ReadLine()!);
 
 Console.Write("Введите количество столбцов массива: ");
-int n = int.Parse(Console.ReadLine ()!);
+int n = int.Parse(Console.ReadLine()!);
 
-int[,]ourArray = GetArray(m,n,-5,23);
-PrintArray(ourArray);
-
-Console.Write("Введите искомое число: ");
-int find = int.Parse(Console.ReadLine ()!);
-
-GetNumFromArray(ourArray,find);
+int[,] Array = GetArray(m, n, -12, 23);
+PrintArray(Array);
+Console.WriteLine();
+AverageInColumn(Array);
