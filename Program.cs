1d906be34c945edef049368a210c8f96cdea7082
RomanBarbarossa <roman.barbarossa@mail.ru>
2023-01-29 11:31:25 +0300

    second task is done

--- Program.cs
+++ Program.cs
@@ -1,51 +1,73 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿// // Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// // m = 3, n = 4.
+// // 0,5 7 -2 -0,2
+// // 1 -3,3 8 -9,9
+// // 8 7,8 -7,1 9
 
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine () !);
 
-// double GetArray (int m , int n, int min,int mares)
-//  {
-//     double[,] res = new double[m,n];
-    
-//     for (int i = 0; i < res.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < res.GetLength(1); j++)
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine ()!);
+
+// double[,] array = GetArray (rows, columns, -5, 10);
+// PrintArray(array);
+
+// // ____________-----Methods-----______________
+// double[,] GetArray(int m, int n, int min, int max)
+// {
+//     double[,] res = new double[m, n];
+//     for (int i = 0; i < n; i++)
 //         {
-//             res[i,j] = Math.Round((new Random().NextDouble()),1);
+//             for (int j = 0; j < m; j++)
+//             {
+//                 res[i, j] = Math.Round((new Random().NextDouble()),2);
+//             }
 //         }
+//         return res;
+// }
+
+
+// void PrintArray(double[,]col)
+// {
+//     for (int i = 0; i < col.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < col.GetLength(1); j++)
+//         {
+//             Console.Write($"{col[i, j]}  ");
+//         }
+//         Console.WriteLine();
 //     }
-//     return res;
-//  }
+// }
 
-// double[,] col = GetArray(4,4,0,10);
+// _________________________________________________________-
 
+// Задача 50. Напишите программу, которая на вход принимает число,и проверяет есть ли 
+// такое число в двумерном массиве, а также возвращает сообщение о том,что оно найдено 
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
 
-Console.Write("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine () !);
+// ____________-----Methods-----______________
 
-Console.Write("Введите количество столбцов массива: ");
-int columns = int.Parse(Console.ReadLine ()!);
-
-double[,] array = GetArray (rows, columns, -5, 10);
-PrintArray(array);
-
-double[,] GetArray(int m, int n, int min, int max)
+//           __1__  создание массива
+int[,] GetArray(int m, int n, int min, int max)
 {
-    double[,] res = new double[m, n];
-    for (int i = 0; i < n; i++)
+    int[,] res = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
         {
-            for (int j = 0; j < m; j++)
-            {
-                res[i, j] = Math.Round((new Random().NextDouble()),2);
-            }
+            res[i, j] = new Random().Next(min, max - 1);
         }
-        return res;
+    }
+    return res;
 }
-
-
-void PrintArray(double[,]col)
+//          __2__ печать данного массива
+void PrintArray(int[,] col)
 {
     for (int i = 0; i < col.GetLength(0); i++)
     {
@@ -56,3 +78,36 @@ void PrintArray(double[,]col)
         Console.WriteLine();
     }
 }
+
+        // __3__ нахождение нужного значения в данном массиве
+
+void GetNumFromArray(int[,] array, int find)
+{
+    bool IsHere = false;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == find) IsHere = true;
+        }
+    }
+    if (IsHere == true)
+    {
+        Console.WriteLine("Число есть в массиве");
+    }
+    else Console.WriteLine("Числа нет в массиве");
+}
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine () !);
+
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine ()!);
+
+int[,]ourArray = GetArray(m,n,-5,23);
+PrintArray(ourArray);
+
+Console.Write("Введите искомое число: ");
+int find = int.Parse(Console.ReadLine ()!);
+
+GetNumFromArray(ourArray,find);
